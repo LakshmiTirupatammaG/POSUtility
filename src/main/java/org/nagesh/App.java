68bf10d8474fdfde5eca79c676d63f0d7d606bef
LakshmiTirupatammaG <lakshmi.garlapati@gmail.com>
2024-07-05 18:27:01 +0530

    Initial version

---
+++ src/main/java/org/nagesh/App.java
@@ -0,0 +1,19 @@
+package org.nagesh;
+
+import static org.nagesh.login.getinput;
+
+/**
+ * Hello worldd!
+ *
+ */
+public class App 
+{
+
+
+
+    public static void main( String[] args )
+    {
+        getinput();
+
+    }
+}
